f5cfca95d5b12167d6efb3fcca669044a7e8e9e8
Dmitry Birin <dmitry.birin@gmail.com>
2016-04-03 21:22:09 +0300

    First commit to GitHub Pages

---
+++ js/app.js
@@ -0,0 +1,45 @@
+(function(){
+
+angular.module('ButtonApp', ['ngMaterial'])
+    .controller('MainCtrl', function() {
+
+    function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    this.buttonName = "Random Episode";
+    this.click = function(){
+        var seasonList = {
+        3:25,
+        6:25,
+        10:17
+        };
+        
+        this.season = getRandomIntInclusive(1,10);
+        this.episode = 0;
+
+        if (seasonList.hasOwnProperty(this.season.toString()))
+        {
+        this.episode = getRandomIntInclusive(1,seasonList[season]);  
+        }
+        else 
+        {
+        this.episode = getRandomIntInclusive(1,24);
+        }
+        
+        this.buttonName = "Try Again";
+        
+        console.log("------FRIENDS RANDOM-------");
+        console.log("Season: " + this.season);
+        console.log("Episode: " +  this.episode);
+
+        var gaLogClick = function () {
+        ga('send', 'event', 'button', 'click', 'RandomEpisode');
+        };
+        
+        gaLogClick();
+            
+    }
+    
+});
+}());

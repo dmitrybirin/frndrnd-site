dcabbe5361b833fa9b0ee9ff61bb0fc8468ed9eb
Dmitry Birin <dmitry.birin@gmail.com>
2018-01-27 20:24:28 +0300

    deleted angular code refactored app.js

--- js/app.js
+++ js/app.js
@@ -1,41 +1,32 @@
 (function(){
 
-angular.module('ButtonApp', ['ngMaterial'])
-    .controller('MainCtrl', function() {
+    const getRandomIntInclusive = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
 
-    function getRandomIntInclusive(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+    const gaLogClick = function () {
+        ga('send', 'event', 'button', 'click', 'RandomEpisode')
+    };
 
-    this.click = function(){
-        var seasonList = {
-        3:25,
-        6:25,
-        10:17
-        };
+    document.querySelector('#button').addEventListener('click', () => {
+        var episodeCount = [ 24, 24, 25, 24, 24, 25, 24, 24, 24, 17 ]
         
-        this.season = getRandomIntInclusive(1,10);
-
-        if (seasonList.hasOwnProperty(this.season.toString()))
-        {
-        this.episode = getRandomIntInclusive(1,seasonList[this.season]);  
-        }
-        else 
-        {
-        this.episode = getRandomIntInclusive(1,24);
-        }
+        this.season = getRandomIntInclusive(1,10)
+        this.episode = getRandomIntInclusive(1,episodeCount[this.season-1])
         
-        console.log("------FRIENDS RANDOM-------");
-        console.log("Season: " + this.season);
-        console.log("Episode: " +  this.episode);
+        console.log(`
+            ------FRIENDS RANDOM-------
+            Season: ${this.season}
+            Episode: ${this.episode}
+        `)
+        const seasonP = document.querySelector('p.results#season')
+        const episodeP = document.querySelector('p.results#episode')
 
-        var gaLogClick = function () {
-        ga('send', 'event', 'button', 'click', 'RandomEpisode');
-        };
-        
-        gaLogClick();
-            
-    }
+        seasonP.innerHTML = `Season: ${this.season}`
+        seasonP.className = `${seasonP.className} white-color`
+        episodeP.innerHTML = `Episode: ${this.episode}`
+        episodeP.className = `${seasonP.className} white-color`
+
+    })
     
-});
+    gaLogClick();
+            
 }());
